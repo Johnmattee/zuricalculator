aa94030946c0024c52dc2af24d0969311628a80b
Johnmattee <42149205+Johnmattee@users.noreply.github.com>
2022-06-25 01:11:27 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+let calculator = {
+add(first, second){
+return first + second;
+} subtract(first, second){
+return first - second;
+} multiply(first, second){
+return first * second;
+} divide(first,second){
+return first/second;
+}
+}
